c39e34e004b719db109cec6f8a9d8d05df645fe1
Sergei <sgus2012@mail.ru>
2022-08-12 23:25:46 +0500

    Добавил изображения, которые будут воспроизводиться

--- Time_cats.py
+++ Time_cats.py
@@ -5,7 +5,7 @@ a = str(input("Что вы делаете? "))
 if a == str("работаю"):
     print("Хорошо, продолжай работать, только недолго!")
 elif a == str("не работаю"):
-    img=Image.open('C:\\Users\\sgus2\\Pictures\\Новая папка\\_20160116_184834.jpg')
+    img=Image.open('C:\\Users\\sgus2\\Desktop\\Время кисяк\\pic\\1.jpg')
     img.show()
 else:
     print("грустно")
